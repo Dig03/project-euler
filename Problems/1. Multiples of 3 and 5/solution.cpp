5cc0c93aa0fb794c15b3cb8c0dcc29ac03d6cf40
Dig <Dig03@users.noreply.github.com>
2017-03-05 17:20:23 +0000

    Remove unnecessary imports and fix algorithm.

--- Problems/1. Multiples of 3 and 5/solution.cpp
+++ Problems/1. Multiples of 3 and 5/solution.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <numeric>
 
 int main()
 {
-    std::vector<int> multiples;
-    for (int i = 0; i < 1000; i++)
-    {
-        if ( (i % 3) == 0 || (i % 5) == 0 )
-        {
-            multiples.push_back(i);
+    for (int i = 0; i < 1000; i++) {
+        if ( i % 3 == 0 || i % 5 == 0 ) {
+            sum += i;
         }
     }
-    int sum = std::accumulate(multiples.begin(), multiples.end(), 0);
     std::cout << sum;
     return 0;
 }
